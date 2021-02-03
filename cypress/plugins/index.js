25a2c5cea4911de5ea5a0974888f4987524e6b77
Gil Tayar <gil@tayar.org>
2021-02-03 08:11:47 +0200

    oops. cypress plugins shouldn't initially be in the repo

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -19,6 +19,3 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
-
-
-require('@applitools/eyes-cypress')(module);
